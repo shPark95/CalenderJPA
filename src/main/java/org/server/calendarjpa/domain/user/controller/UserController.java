968864c9eb266324a25c8f7a21fb31f7d86ecc1b
박성현 <110206842+shPark95@users.noreply.github.com>
2025-05-22 18:33:49 +0900

    Add: feature/user CRUD

--- src/main/java/org/server/calendarjpa/domain/user/controller/UserController.java
+++ src/main/java/org/server/calendarjpa/domain/user/controller/UserController.java
@@ -1,8 +1,11 @@
 package org.server.calendarjpa.domain.user.controller;
 
+import org.server.calendarjpa.domain.user.dto.UserRequestDto;
+import org.server.calendarjpa.domain.user.dto.UserResponseDto;
 import org.server.calendarjpa.domain.user.service.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -11,5 +14,31 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+
+    @PostMapping("/users")
+    public UserResponseDto create(@RequestBody UserRequestDto userRequestDto) {
+        return userService.create(userRequestDto);
+    }
+
+    @GetMapping("/users")
+    public List<UserResponseDto> findAll() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public UserResponseDto findById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    @PatchMapping("/users/{id}")
+    public UserResponseDto update(@PathVariable Long id, @RequestBody UserRequestDto userRequestDto) {
+        return userService.update(id, userRequestDto);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteById(@PathVariable Long id) {
+        userService.deleteById(id);
     }
 }
--- src/main/java/org/server/calendarjpa/domain/user/service/UserService.java
+++ src/main/java/org/server/calendarjpa/domain/user/service/UserService.java
@@ -1,7 +1,14 @@
 package org.server.calendarjpa.domain.user.service;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.server.calendarjpa.domain.user.dto.UserRequestDto;
+import org.server.calendarjpa.domain.user.dto.UserResponseDto;
+import org.server.calendarjpa.domain.user.entity.User;
 import org.server.calendarjpa.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -9,5 +16,32 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public UserResponseDto create(UserRequestDto userRequestDto) {
+        User user = User.of(userRequestDto.getUsername(), userRequestDto.getPassword(), userRequestDto.getEmail());
+        return new UserResponseDto(userRepository.save(user));
+    }
+
+    public UserResponseDto findById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+        return new UserResponseDto(user);
+    }
+
+    public List<UserResponseDto> findAll() {
+        return userRepository.findAll().stream()
+                .map(UserResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    public UserResponseDto update(Long id, UserRequestDto userRequestDto) {
+        User user = userRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+        user = User.of(userRequestDto.getUsername(), userRequestDto.getPassword(), userRequestDto.getEmail());
+        userRepository.save(user);
+        return new UserResponseDto(user);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
     }
 }
